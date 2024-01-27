197c52d1fe59fdfcb9d49cfaa911ea39e411970b
Michael Feldman <michael.feldman09@gmail.com>
2024-01-26 23:48:39 -0500

    Simply horizontal movement a bunch
    Basically just lerp to target speed.
    Also experimented with some imgui stuff for sliders in the game window.
    Not sure if will continue with those...

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,58 +4,28 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
-    private Vector2 _velocity;
-    public AnimationCurve horAccelCurve;
-    public float horAccelDur;
-    private float _horAccelTimer;
     public float maxHorVel;
+    public float horLerp;
+    private Vector2 _velocity;
 
-    public enum HorMoveState {
-        Idle,
-        Accel,
-        FullSpeed,
-        Decel
+    void OnGUI() {
+        GUI.Box(new Rect(10,10,120,90), "Character Settings");
+        horLerp = GUI.HorizontalSlider (new Rect (15, 35, 100, 30), horLerp, 0.0f, 400.0f);
     }
-    public HorMoveState horMoveState;
+
     // Start is called before the first frame update
     void Start()
     {
-        horMoveState = HorMoveState.Idle;
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         float horInput = Input.GetAxis("Horizontal");
-
-        switch(horMoveState) {
-            case HorMoveState.Idle:
-                if(horInput != 0) {
-                    horMoveState = HorMoveState.Accel;
-                    _horAccelTimer += Time.deltaTime;
-                    _velocity.x = horAccelCurve.Evaluate(_horAccelTimer / horAccelDur)
-                        * maxHorVel;
-                }
-                break;
-            case HorMoveState.Accel:
-                if(horInput != 0) {
-                    horMoveState = HorMoveState.Accel;
-                    _horAccelTimer += Time.deltaTime;
-                    if(_horAccelTimer >= horAccelDur) {
-                        _velocity.x = maxHorVel;
-                        horMoveState = HorMoveState.FullSpeed;
-                    }
-                    else {
-                        _velocity.x = horAccelCurve.Evaluate(_horAccelTimer / horAccelDur)
-                            * maxHorVel;
-                    }
-                }
-                break;
-            case HorMoveState.FullSpeed:
-                break;
-        }
+        _velocity.x = Mathf.Lerp(_velocity.x, horInput * maxHorVel, Time.deltaTime * horLerp);
 
         transform.position += Vector3.right * _velocity.x * Time.deltaTime;
     }
+
 }
