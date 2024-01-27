84d6a1754eb0567c412e7c9a5a1e5799a075418d
Michael Feldman <michael.feldman09@gmail.com>
2024-01-27 15:08:34 -0500

    Fix issue with jump not resetting
    Use a coroutine to check for y velocity in order to check for grounded.
    Also changed horizontal movement to set velocity instead of setting
    position.
    Fixed an issue where the collider stuck to wall by doing a capsule
    overlap check and if so, set the horizontal velocity to 0

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,6 +10,7 @@ public class CharacterController : MonoBehaviour
     private Vector2 _velocity;
     private bool _grounded;
     private Rigidbody2D _rigidbody;
+    private CapsuleCollider2D _collider;
 
     //these arrows are for debugging avatar direction
     [HideInInspector]
@@ -42,6 +43,7 @@ public class CharacterController : MonoBehaviour
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _collider = GetComponent<CapsuleCollider2D>();
         //this may be a bit naive, but let's assume the player always spawns on solid ground
         _grounded = true;
 
@@ -65,9 +67,29 @@ public class CharacterController : MonoBehaviour
     {
         //horizontal motion
         float horInput = Input.GetAxis("Horizontal");
+        /*
         _velocity.x = Mathf.Lerp(_velocity.x, horInput * settings.maxHorVel, 
                 Time.deltaTime * settings.horLerp);
         transform.position += Vector3.right * _velocity.x * Time.deltaTime;
+        */
+        Vector2 vel = _rigidbody.velocity;
+        vel.x = horInput * settings.maxHorVel;
+
+        //test position before applying otherwise we get stuck in wall
+        Vector2 newPos = new Vector2(transform.position.x, transform.position.y)
+            + vel * Time.deltaTime * 1.5f + 
+            Vector2.up * transform.localScale.y * _collider.size.y * 0.5f;
+
+        Collider2D hit = Physics2D.OverlapCapsule(newPos, 
+                new Vector2(_collider.size.x * transform.localScale.x,
+                    _collider.size.y * transform.localScale.y * 0.9f), 
+                CapsuleDirection2D.Vertical, 0);//, _collisionLayerMask);
+
+        if(hit != null) {
+            vel.x = 0;
+        }
+        _rigidbody.velocity = vel;
+
         //vis dir
         if(horInput != 0) {
             goingRight = horInput > 0;
@@ -79,6 +101,7 @@ public class CharacterController : MonoBehaviour
         if(_grounded && Input.GetButtonDown("Jump")) {
             _grounded = false;
             _rigidbody.AddForce(Vector2.up * settings.jumpForce);
+            StartCoroutine(CheckForGround());
         }
         else if(!_grounded) {
             //downward force to spice up the jump
@@ -86,11 +109,22 @@ public class CharacterController : MonoBehaviour
         }
     }
 
+    IEnumerator CheckForGround() {
+        //do we need to wait some frames for velocity to update
+        yield return new WaitForSeconds(0.5f);
+        while(_rigidbody.velocity.y != 0) {
+            yield return null;
+        }
+        _grounded = true;
+    }
+
+    /*
     void OnCollisionEnter2D(Collision2D other){
         if(_grounded == true) {
             //we only use this callback to check if the player is grounded for now...
             return;
         }
+
 
         //unity recommends against using this because of Garbage generation
         //but we can worry about that later
@@ -101,4 +135,5 @@ public class CharacterController : MonoBehaviour
             }
         }
     }
+    */
 }
