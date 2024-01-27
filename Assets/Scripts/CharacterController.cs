ac6fb3feabbd221585809fdb5850befda10713d4
Michael Feldman <michael.feldman09@gmail.com>
2024-01-26 22:29:19 -0500

    First pass at horizontal acceleration
    Trying a state machine, but this is proving kind of unweildy. Will need
    to think about a more robust way to handle the transitions between
    acceleration, full speed, decel, and a model that accounts better for
    moving left and right

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -4,20 +4,58 @@ using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
+    private Vector2 _velocity;
+    public AnimationCurve horAccelCurve;
+    public float horAccelDur;
+    private float _horAccelTimer;
+    public float maxHorVel;
+
+    public enum HorMoveState {
+        Idle,
+        Accel,
+        FullSpeed,
+        Decel
+    }
+    public HorMoveState horMoveState;
     // Start is called before the first frame update
     void Start()
     {
+        horMoveState = HorMoveState.Idle;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.A)) {
-            transform.position += Vector3.left * Time.deltaTime;
+        float horInput = Input.GetAxis("Horizontal");
+
+        switch(horMoveState) {
+            case HorMoveState.Idle:
+                if(horInput != 0) {
+                    horMoveState = HorMoveState.Accel;
+                    _horAccelTimer += Time.deltaTime;
+                    _velocity.x = horAccelCurve.Evaluate(_horAccelTimer / horAccelDur)
+                        * maxHorVel;
+                }
+                break;
+            case HorMoveState.Accel:
+                if(horInput != 0) {
+                    horMoveState = HorMoveState.Accel;
+                    _horAccelTimer += Time.deltaTime;
+                    if(_horAccelTimer >= horAccelDur) {
+                        _velocity.x = maxHorVel;
+                        horMoveState = HorMoveState.FullSpeed;
+                    }
+                    else {
+                        _velocity.x = horAccelCurve.Evaluate(_horAccelTimer / horAccelDur)
+                            * maxHorVel;
+                    }
+                }
+                break;
+            case HorMoveState.FullSpeed:
+                break;
         }
-        if(Input.GetKey(KeyCode.D)) {
-            transform.position += Vector3.right * Time.deltaTime;
-        }
+
+        transform.position += Vector3.right * _velocity.x * Time.deltaTime;
     }
 }
