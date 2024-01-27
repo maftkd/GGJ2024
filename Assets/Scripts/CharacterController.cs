afe916fa66b07f1e8aa5828bba0b0887fe59f384
Michael Feldman <michael.feldman09@gmail.com>
2024-01-27 00:29:49 -0500

    First pass at jumping
    Basically just applying a force.
    Air control is a little weird and the jump arc feels a bit canned.
    Getting some decent use out of the gui stuff, but would be better if we
    could serialize after play mode

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -7,10 +7,23 @@ public class CharacterController : MonoBehaviour
     public float maxHorVel;
     public float horLerp;
     private Vector2 _velocity;
+    private bool _grounded;
+    private Rigidbody2D _rigidbody;
+    public float jumpForce;
 
     void OnGUI() {
-        GUI.Box(new Rect(10,10,120,90), "Character Settings");
-        horLerp = GUI.HorizontalSlider (new Rect (15, 35, 100, 30), horLerp, 0.0f, 400.0f);
+        GUI.Box(new Rect(10,10,120,90), "Character Info");
+        GUI.Label (new Rect (15, 35, 100, 30), "Grounded: " + _grounded);
+
+        GUI.Label (new Rect (15, 55, 100, 30), "Jump Force: " + jumpForce.ToString("0"));
+        jumpForce = GUI.HorizontalSlider(new Rect(15, 75, 100, 30), jumpForce, 0f, 1000f);
+    }
+
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        //this may be a bit naive, but let's assume the player always spawns on solid ground
+        _grounded = true;
     }
 
     // Start is called before the first frame update
@@ -22,10 +35,32 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //horizontal motion
         float horInput = Input.GetAxis("Horizontal");
         _velocity.x = Mathf.Lerp(_velocity.x, horInput * maxHorVel, Time.deltaTime * horLerp);
-
         transform.position += Vector3.right * _velocity.x * Time.deltaTime;
+
+        //jump
+        if(_grounded && Input.GetButtonDown("Jump")) {
+            _grounded = false;
+            _rigidbody.AddForce(Vector2.up * jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(_grounded == true) {
+            //we only use this callback to check if the player is grounded for now...
+            return;
+        }
+
+        //unity recommends against using this because of Garbage generation
+        //but we can worry about that later
+        foreach(ContactPoint2D contactPoint in other.contacts) {
+            if(Vector2.Dot(contactPoint.normal, Vector2.up) > 0) {
+                _grounded = true;
+                return;
+            }
+        }
     }
 
 }
